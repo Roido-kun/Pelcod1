14a2c25c41610b39cfd8b9430180a8d36a37e496
Roido-kun <143859853+Roido-kun@users.noreply.github.com>
2023-09-01 20:59:30 -0700

    Add files via upload
    ini pelatihan coding pertama

---
+++ latihan1.py
@@ -0,0 +1,26 @@
+nama_saya = str(input("masukan nama anda = "))
+print(nama_saya, "bertipe", type(nama_saya))
+
+umur = int(input("masukan umur anda = "))
+print(umur, "bertipe", type(umur))
+
+IPK = float(input("masukan IPK anda = "))
+print(IPK, "bertipe", type(umur))
+
+menikah = bool(input("sudah menikah? ="))
+print("menikah?",menikah, "bertipe", type(menikah))
+
+# matkul_diambil = ["alpro", "sig"]
+# print("matkul : ",matkul_diambil, "bertipe", type(matkul_diambil))
+
+# matkul_diambil2 = ("web", "pemvis")
+# print("matkul lain: ",matkul_diambil2, "bertipe", type(matkul_diambil2))
+
+# matkul_diambil3 = {"Alpro", "Alpro", "pemweb", "data science", "data science"}
+# print("matkul lain-lain: ",matkul_diambil3, "bertipe", type(matkul_diambil3))
+
+# print(1+2)
+# print(11-3)
+# print("1" + "1")
+# print(5*7)
+# print(17/3)
